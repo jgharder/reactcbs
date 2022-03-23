ec4de2c89f5cf49acf9e2e2eb214bd271326cc07
JGHarder <Jacob.gade.harder@gmail.com>
2022-03-23 16:37:14 +0100

    r du glad Jimmi?

--- components/index.tsx
+++ components/index.tsx
@@ -1,16 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { useSelector } from "react-redux";
-import ChatStackNavigator from "../components/navigators/ChatStackNavigator"
-import SignInStackNavigator from "../components/navigators/SignInStackNavigator"
-import { User } from "../entities/User";
+import ChatStackNavigator from "../components/navigators/ChatStackNavigator";
+import SignInStackNavigator from "../components/navigators/SignInStackNavigator";
 
 const index = () => {
   const user = useSelector((state: any) => state.user.loggedInUser);
 
-return <> 
-    {user? <ChatStackNavigator/>: <SignInStackNavigator/> }</>
-    
+  return <> {user ? <ChatStackNavigator /> : <SignInStackNavigator />}</>;
 };
 
 export default index;
