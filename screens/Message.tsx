9497593e8a7c0293ab3ec11828cdd19053f2a91b
AndreasHolmAndersen <andreas.holm.andersen@gmail.com>
2022-04-29 10:53:27 +0200

    finished chatroom with messages

--- screens/Message.tsx
+++ screens/Message.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import {
-  Button,
   FlatList,
+  Pressable,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -13,66 +13,131 @@ import { addMessage, fetchMessages } from "../store/actions/chat.actions";
 import { Message, Status } from "../entities/Message";
 import { Chatroom } from "../entities/Chatroom";
 
-  const MessageScreen = (props: any) =>{
+const MessageScreen = (props: any) => {
   const dispatch = useDispatch();
   const [title, onChangeTitle] = React.useState("");
   const [text, onChangeText] = React.useState("");
 
-  const {id} = props.route.params;
-  
+  const { id } = props.route.params;
+
   useEffect(() => {
     dispatch(fetchMessages(id));
   }, []);
 
-  const chatMessages = useSelector((state: any ) => state.chat.chatrooms).find((room:Chatroom) => room.id == id).messages;
-  // console.log(id)
-    // console.log(useSelector((state: any ) => state.chat));
-    // console.log(useSelector((state: any) => state.chat.chatrooms).find((room:Chatroom)=>room.id === id))
-  console.log(chatMessages);
-  
+  const chatMessages = useSelector((state: any) => state.chat.chatrooms).find((room: Chatroom) => room.id == id).messages;
+
+
+
 
   const handleAddMessage = () => {
     const message: Message = new Message(
-      title, Status.UNREAD,text, new Date())
+      title, Status.UNREAD, text, new Date())
     dispatch(addMessage(id, message));
   };
 
   const renderMessage = ({ item }: { item: Message }) => (
-    <>
-      <Text>{item.title}</Text>
-      <Text>{item.status}</Text>
-      <Text>
-        {item.timestamp.getDate()}/{item.timestamp.getMonth()}/
-        {item.timestamp.getFullYear()}
+    <View style={styles.messageContainer}>
+      <Text style={styles.textTitle}>{item.title}</Text>
+      <Text style={styles.time}>{item.timestamp.getHours()}:{item.timestamp.getMinutes().toString().padStart(2, '0')}</Text>
+      <Text style={styles.timeStamp}>
+        {item.timestamp.getDate()}/{item.timestamp.getMonth()}/{item.timestamp.getFullYear()}
       </Text>
-    </>
+    </View>
   );
 
   return (
     <>
       <SafeAreaView style={styles.container}>
-      {chatMessages? <Text>No messages</Text>:  <FlatList
-          data={chatMessages}
-          renderItem={renderMessage}
-          keyExtractor={(item:any) => item.id}
-        />}
-        <TextInput
-          onChangeText={onChangeTitle}
-          value={title}
-          placeholder="Write new message"
-        />
-        <Button title="Submit" onPress={handleAddMessage} />
+        <View style={styles.flatList}>
+
+          <FlatList
+            data={chatMessages}
+            renderItem={renderMessage}
+            keyExtractor={(item: any) => item.id}
+          />
+        </View>
+        <View style={styles.textInputAndBtn}>
+          <TextInput
+            style={styles.messageInput}
+            onChangeText={onChangeTitle}
+            value={title}
+            placeholder="Write new message"
+          />
+          <Pressable style={styles.messageInputBtn} onPress={handleAddMessage}><Text>Send Message</Text></Pressable>
+        </View>
       </SafeAreaView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  flatList:{
+    flex: 6,
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+
+  textInputAndBtn: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+  },
+
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+
+  messageContainer: {
+    flex: 1,
+    backgroundColor: "#5050A5",
+    margin: 10,
+    borderRadius: 5,
+    minHeight: 50,
+    height: "auto",
+    maxWidth: 200,
+    paddingVertical: 10,
+  },
+
+  textTitle: {
+    fontSize: 16,
+    paddingLeft: 10,
+    maxWidth: "70%",
+  },
+
+  timeStamp: {
+    position: "absolute",
+    opacity: 0.5,
+    right: 0,
+    bottom: 0,
+    paddingBottom: 2,
+    paddingRight: 4,
+    fontSize: 10,
+  },
+
+  time: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    paddingRight: 4,
+    paddingTop: 2,
+    opacity: 0.7,
+    fontSize: 12,
+  },
+
+  messageInput: {
+    paddingVertical: 10, 
+    textAlign: "center",
+    borderTopWidth: 1,
+    width: "100%",
+  },
+
+  messageInputBtn: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "grey",
+    marginBottom: 10,
   },
 });
 
--- store/actions/chat.actions.ts
+++ store/actions/chat.actions.ts
@@ -3,6 +3,9 @@ import { Message } from "../../entities/Message";
 
 export const ADD_CHATROOM = "ADD_CHATROOM";
 export const FETCH_CHATROOMS = "FETCH_CHATROOMS";
+export const FETCH_MESSAGES = "FETCH_MESSAGES";
+export const ADD_MESSAGE = "ADD_MESSAGE";
+
 
 export const fetchChatrooms = () => {
   return async (dispatch: any, getState: any) => {
@@ -28,11 +31,18 @@ export const fetchChatrooms = () => {
       let chatrooms: Chatroom[] = [];
       for (const key in data) {
         let obj = data[key];
+        let messages = [];
+        for (const key2 in obj.messages) {
+          let message = obj.messages[key2];
+          messages.push(new Message(message.title, message.status, message.text, new Date(message.timestamp), key2));
+        }
         // create Chatroom objects and push them into the array chatrooms.
         chatrooms.push(
-          new Chatroom(obj.title, [], new Date(obj.timestamp), key)
+          new Chatroom(obj.title, messages, new Date(obj.timestamp), key)
         );
       }
+
+      
 
       // console.log("data from server", chatrooms);
 
@@ -85,8 +95,6 @@ export const addChatroom = (chatroom: Chatroom) => {
   };
 };
 
-export const FETCH_MESSAGES = "FETCH_MESSAGES";
-export const ADD_MESSAGE = "ADD_MESSAGE";
 
 export const fetchMessages = (id: string) => {
   return async (dispatch: any, getState: any) => {
